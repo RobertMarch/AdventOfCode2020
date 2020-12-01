5ba85bb6101ecd563eaa05893f57d81f24469c53
Rob March <robertmarch5@gmail.com>
2020-12-01 19:47:17 +0000

    Create template files

---
+++ TestRunner.py
@@ -0,0 +1,14 @@
+def run_tests(test_cases, solve):
+    failed_test = False
+    for case in test_cases:
+        [expected_answer, case_input] = case
+        solution = solve(case_input)
+        if not solution == expected_answer:
+            print('Error in test case: expected {}, got {} for input {}'.format(expected_answer, solution, case_input))
+            failed_test = True
+    if len(test_cases) == 0:
+        print('No test cases given. \n')
+    elif not failed_test:
+        print('All test cases passed! \n')
+    else:
+        print('\n')
---
+++ adventXXa.py
@@ -0,0 +1,11 @@
+from TestRunner import run_tests
+from inputs.inputXX import puzzle_input, test_cases_a
+
+
+def solve(input):
+    return 1
+
+
+if __name__ == "__main__":
+    run_tests(test_cases_a, solve)
+    print(solve(puzzle_input))
